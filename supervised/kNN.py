0179e4f5d7ff7734944ee6ed8ef568c4f6e24456
haiqiong <yaohaiqiong@gmail.com>
2012-06-27 15:54:36 -0700

    some changes

--- supervised/kNN.py
+++ supervised/kNN.py
@@ -28,5 +28,74 @@ def classify0(inX, dataSet, labels, k):
             operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    
+    #reopen the file to process each line.
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        #populate returnMat, returnMat is numberofLines X 3
+        returnMat[index, :] = listFromLine[0:3]
+        # classLabelVector is numberOfLines X 1
+        classLabelVector.append(listFromLine[-1])
+        index += 1
+    return returnMat, classLabelVector
 
+def autoNorm(dataSet):
+    #get the min and max (1 X 3)from column.
+    minValues = dataSet.min(0)
+    maxValues = dataSet.max(0)
+    ranges = maxValues - minValues
+    
+    m = dataSet.shape[0]
+    #expand minValues (1X3) to m rows.
+    normDataSet = dataSet - tile(minValues, (m, 1))
+    #/ is element-wise division
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    
+    return normDataSet, ranges, minValues
+
+#10% data of dataset is used to test the accuracy of the classifier.
+def datingClassTest():
+    testRatio = 0.10
+    datingDataSet, datingLabels = file2matrix('../dataset/datingTestSet.txt')
+    normDataSet, ranges, minVals = autoNorm(datingDataSet)
+    
+    m = normDataSet.shape[0]
+    trainStart = int(m * testRatio)
+    errorCount = 0.0
+    
+    for i in range(trainStart):
+        # k = 3.
+        classifier = classify0(normDataSet[i, :], normDataSet[trainStart:m, :],\
+                               datingLabels[trainStart:m], 3)
+        print "the classifier is: %s, the real class is: %s" \
+              % (classifier, datingLabels[i])
+        if (cmp(classifier, datingLabels[i]) != 0):
+            errorCount += 1.0
+    
+    print 'the total error rate is: %f' % (errorCount / float(trainStart))
+    
+#user interface 
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentGame = float(raw_input('percentage of time spent playing \
+    video games?'))
+    flyMiles = float(raw_input('frequent fly miles earned per year?'))
+    iceCream = float(raw_input('liters of ice cream consumed per year?'))
+    
+    datingDataSet, datingLabels = file2matrix('../dataset/datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataSet)
+    queryArr = array([flyMiles, percentGame, iceCream])
+    queryNorm = (queryArr - minVals) / ranges
+    
+    k = 3
+    classifier = classify0(queryNorm, normMat, datingLabels, k)
+    print 'You will probably like this person: ', classifier
 
